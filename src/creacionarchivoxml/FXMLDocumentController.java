1022dd3250fc818391c8a0c2c460082e6e9798c3
gabriel.mercado <gabriel.mercado@SCG-18V-D59622.uao.edu.co>
2018-08-08 14:58:48 -0500

    cree una interfaz para agregar empleados con dos botones

--- src/creacionarchivoxml/FXMLDocumentController.java
+++ src/creacionarchivoxml/FXMLDocumentController.java
@@ -6,30 +6,41 @@
 package creacionarchivoxml;
 
 import java.net.URL;
+import java.util.LinkedList;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import modelo.Empleado;
 
 /**
  *
  * @author gabriel.mercado
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
-    private Label label;
-    
+    private Label ltitulo, lnombre, ldireccion, ledad, lcodigo, ldepartamento;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private TextField tnombre, tdireccion, tedad, tcodigo, tdepartamento;
+    LinkedList<Empleado> listaEmpleado;
+
+    @FXML
+    private void agregarEmpleado(ActionEvent event) {
+        String nombre = tnombre.getText();
+        String direccion = tdireccion.getText();
+        int edad = Integer.parseInt(tedad.getText());
+        String codigo = tcodigo.getText();
+        String departamento = tdepartamento.getText();
+        listaEmpleado.add(new Empleado(codigo, departamento, nombre, direccion, edad));
+        
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        listaEmpleado = new LinkedList<>();
+    }
+
 }
